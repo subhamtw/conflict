47123ddd4f5027b5c02428582962b226cdbf1356
subhamtw <subham.kumar@thoughtworks.com>
2021-02-08 00:09:26 +0530

    Switch

--- src/hello.java
+++ src/hello.java
@@ -1,15 +1,21 @@
+import java.util.Random;
+
 public class hello {
     public static void main(String[] args) {
         System.out.println("Hello World");
-        if(2+2 == 4)
-            System.out.println("Correct you are genius");
-        else
-        {
-            System.out.println("Repeat your Career from schooling ;) ");
-            for(int i=1;i<=12;i++)
-                System.out.println(i);
+        Random random = new Random();
+        int number = random.nextInt(3);
+        switch(number){
+            case 1:
+                System.out.println("Apple");
+                break;
+            case 2:
+                System.out.println("Mango");
+                break;
+            default:
+                System.out.println("Better Luck Next Time");
         }
 
-        System.out.println("Finally you are correct");
+        System.out.println("This is shubham");
     }
 }
