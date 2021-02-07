7a1d374d2b8a57cbd1a56ca361a384b74c69c40d
subhamtw <subham.kumar@thoughtworks.com>
2021-02-08 00:32:04 +0530

    Point AWs

--- src/hello.java
+++ src/hello.java
@@ -1,31 +1,11 @@
-import java.util.Objects;
-import java.util.Random;
-class Point {
-    private final int x;
-    private final int y;
+import java.awt.*;
 
-    public Point(int x, int y) {
-        this.x = x;
-        this.y = y;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Point point = (Point) o;
-        return x == point.x &&
-                y == point.y;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(x, y);
-    }
-}
 public class hello {
     public static void main(String[] args) {
-        Point p = new Point(1,2);
-        System.out.println(p.toString());
+        Point point = new Point();
+        point.x =10;
+        point.y=20;
+
+        System.out.println(point.toString());
     }
 }
