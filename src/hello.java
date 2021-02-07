7e05752cba36f27dba0d405dc7cea91b5b97ba02
subhamtw <subham.kumar@thoughtworks.com>
2021-02-08 00:04:47 +0530

    Funny Code

--- src/hello.java
+++ src/hello.java
@@ -1,5 +1,15 @@
 public class hello {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        if(2+2 == 4)
+            System.out.println("Correct you are genius");
+        else
+        {
+            System.out.println("Repeat your Career from schooling ;) ");
+            for(int i=1;i<=12;i++)
+                System.out.println(i);
+        }
+
+        System.out.println("Finally you are correct");
     }
 }
